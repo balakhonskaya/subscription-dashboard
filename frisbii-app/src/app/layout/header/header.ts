87d20aff70ddf9c3066df27df12e7715be230245
Elizaveta B <anthemion@yandex.ru>
2026-03-19 16:04:58 +0100

    Add layout components

---
+++ frisbii-app/src/app/layout/header/header.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Navigation } from "../navigation/navigation";
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  imports: [Navigation, MatToolbarModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+
+}
---
+++ frisbii-app/src/app/layout/main-layout/main-layout.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, Header, Footer, MatSidenavModule],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css',
+})
+export class MainLayout {
+
+}
