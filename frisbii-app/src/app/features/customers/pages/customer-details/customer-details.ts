909d5361f6a3a29d8140e3fd2238a482c3329cd0
Elizaveta B <anthemion@yandex.ru>
2026-03-22 14:58:58 +0100

    Use card component

--- frisbii-app/src/app/features/customers/pages/customer-details/customer-details.ts
+++ frisbii-app/src/app/features/customers/pages/customer-details/customer-details.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Customer } from '../../models/customer.model';
 import { CardVM } from '../../../../shared/card/card.model';
 import { ActivatedRoute } from '@angular/router';
@@ -10,17 +10,15 @@ import { Card } from '../../../../shared/card/card';
   templateUrl: './customer-details.html',
   styleUrl: './customer-details.scss',
 })
-export class CustomerDetails {
+export class CustomerDetails implements OnInit{
 
-  customer = signal<Customer>({handle: 'cust001', first_name: 'John', last_name: 'Doe', email: 'john.doe@example.com'} as Customer);
+  customer = signal<Customer| null>(null);
 
   route = inject(ActivatedRoute);
 
-
-/*
   ngOnInit() {
     this.customer.set(this.route.snapshot.data["customer"]);
-  }*/
+  }
 
    customerToCardVM(customer: Customer): CardVM {
           return {
@@ -29,7 +27,8 @@ export class CustomerDetails {
             card_email: customer.email,
             card_country: customer.country,
             card_company: customer.company,
-            card_address_line1: customer.postal_code + ' ' + customer.address + ' ' + customer.address2,
+            card_postal_code: customer.postal_code,
+            card_address_line1: customer.address + ' ' + customer.address2,
             card_address_line2: customer.city,
           };
         }
--- frisbii-app/src/app/shared/card/card.model.ts
+++ frisbii-app/src/app/shared/card/card.model.ts
@@ -3,6 +3,7 @@ export interface CardVM {
   card_full_name: string;
   card_company?: string;
   card_email?: string;
+  card_postal_code?: string;
   card_address_line1?: string;
   card_address_line2?: string;
   card_country?: string;
